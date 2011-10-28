881dcfa3786e69a18014e0d1a8f0fe33285e4c97
Dafydd Francis <DaveOrdref@gmail.com>
2011-10-28 17:25:48 +0000

    SQLITE Y U NO WORK?

--- lib/haze.rb
+++ lib/haze.rb
@@ -9,7 +9,7 @@ require 'sequel'
 module Haze
   extend self
 
-  DB = Sequel.connect 'sqlite://comments.db'
+  DB = Sequel.connect 'comments.db'
 
   DB.create_table? :comments do
     primary_key :id
