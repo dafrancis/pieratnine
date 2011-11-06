d1942fc2f9a7a0a761686d4fdad083a61478e39e
Dafydd Francis <DaveOrdref@gmail.com>
2011-11-06 18:48:42 +0000

    editing a few niggling things

--- static/gay_babies.rb
+++ static/gay_babies.rb
@@ -4,7 +4,9 @@ require 'open-uri'
 
 class GayBabies
   def self.check_url(key, url)
-    open("https://sb-ssl.google.com/safebrowsing/api/lookup?client=demo-app&apikey=#{key}&appver=1.5.2&pver=3.0&url=#{URI.escape(url)}").read
+    open("https://sb-ssl.google.com/safebrowsing/api/lookup?
+          client=demo-app&apikey=#{key}&appver=1.5.2&pver=3.0
+          &url=#{URI.escape(url)}").read
   end
 end
 </code></pre>
