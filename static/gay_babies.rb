e47b460840995d3b185874b595b19dcf33ce0cdc
Dafydd Francis <DaveOrdref@gmail.com>
2011-11-06 18:41:44 +0000

    new blogpost whathaveyouexplained

---
+++ static/gay_babies.rb
@@ -0,0 +1,8 @@
+require 'uri'
+require 'open-uri'
+
+class GayBabies
+  def self.check_url(key, url)
+    open("https://sb-ssl.google.com/safebrowsing/api/lookup?client=demo-app&apikey=#{key}&appver=1.5.2&pver=3.0&url=#{URI.escape(url)}").read
+  end
+end
