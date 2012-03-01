e7a7b5334cc822e0917188d39e4ac4d575f2ab30
Dafydd Francis <DaveOrdref@gmail.com>
2012-03-01 10:34:02 +0000

    give actual instructions for linking

--- lib/bix.rb
+++ lib/bix.rb
@@ -4,11 +4,14 @@ class BixApp < Sinatra::Base
   set :root, File.expand_path('../../', __FILE__)
   set :views, root + '/views/bix'
 
+  URL_ROOT = '/bix'
+
   def render_erb(template, options={})
     erb(template, options.merge(:layout => !request.xhr?))
   end
 
   get '/' do
+    redirect URL_ROOT + params[:"_escaped_fragment_"] if params[:"_escaped_fragment_"]
     render_erb :home
   end
 
