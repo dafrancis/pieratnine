5b5b2b46e12437677a1f13f044b9608ce47ed3c4
Dafydd Francis <DaveOrdref@gmail.com>
2012-03-29 10:31:56 +0100

    Adding the jaunty angles even if I don't use them on Something Afal

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,3 +1,16 @@
 $(function () {
-  $('time.timeago').timeago();
+  $('time.timeago').each(function () {
+      this.innerHTML = Date.create(this.innerHTML).relative()
+  });
+
+  $(".date").each(function () {
+      this.innerHTML = Date.create(this.innerHTML).format('<span class="daybig">{dd}</span> {Mon} {yyyy}')
+  });
+
+  $(".article").tooltip({ 
+      track: true, 
+      delay: 0,
+      showURL: false,
+      fade: 250 
+  });
 });
