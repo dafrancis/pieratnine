7fa74c1432fae823465867c4d0d78846b04aedf8
Dafydd Francis <daveordref@gmail.com>
2012-11-09 22:07:09 +0000

    remove slur and add mmosim

--- public/static/dumb_babies.rb
+++ public/static/dumb_babies.rb
@@ -1,12 +1,10 @@
-<pre><code>
 require 'uri'
 require 'open-uri'
 
-class GayBabies
+class DumbBabies
   def self.check_url(key, url)
     open("https://sb-ssl.google.com/safebrowsing/api/lookup?
           client=demo-app&apikey=#{key}&appver=1.5.2&pver=3.0
           &url=#{URI.escape(url)}").read
   end
 end
-</code></pre>
